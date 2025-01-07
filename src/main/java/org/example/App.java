bc1073c98882d96103eda46a8299bb21008e4274
kdluu <khanhluu101@gmail.com>
2025-01-07 17:06:12 -0500

    Added configuration file, beans

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+//        Dev dev = new Dev();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Dev dev = applicationContext.getBean(Dev.class);
+        dev.build();
     }
 }
---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Dev {
+
+    public void build() {
+
+        System.out.println("Working on Awesome project!");
+    }
+}
